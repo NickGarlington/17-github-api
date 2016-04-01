1845289f240a0b14efa915f8cabbb0492bc688f9
Nick Garlington <nickgarlington@Nicks-MacBook-Pro-2.local>
2016-04-01 16:18:13 -0500

    final

--- app/app.js
+++ app/app.js
@@ -19,6 +19,55 @@ fetch(" https://api.github.com/users/nickgarlington")
 
     let email = document.querySelector("#email")
     email.textContent = response.email;
-    email.href = `mailto: ${response.email}`;
-    document.querySelector("#email").textContent = response.email;
+    email.href = `mailto:${response.email}`;
   })
+
+  fetch("https://api.github.com/users/nickgarlington/repos")
+  .then((response) => {
+    return response.json();
+  })
+  .then((response) => {
+    console.log(response);
+
+    response.forEach((repo) => {
+        let row = document.createElement("div");
+        row.classList.add("repo");
+
+        let repoName = document.createElement("a");
+        repoName.textContent = repo.name;
+        repoName.href = repo.html_url;
+
+        let info = document.createElement("div");
+        info.classList.add("info");
+
+        let language = document.createTextNode(repo.language);
+        info.appendChild(language);
+
+        let star = document.createElement("i");
+        star.classList.add("fa");
+        star.classList.add("fa-star");
+        info.appendChild(star);
+
+        let stargazers = document.createTextNode(repo.stargazers_count);
+        info.appendChild(stargazers);
+
+        let fork = document.createElement("i");
+        fork.classList.add("fa");
+        fork.classList.add("fa-code-fork");
+        info.appendChild(fork);
+
+
+
+        let forks = document.createTextNode(repo.forks_count);
+        info.appendChild(forks);
+
+        row.appendChild(info);
+        row.appendChild(repoName);
+
+        document.querySelector(".panel1").appendChild(row);
+
+
+
+
+    })
+  });
