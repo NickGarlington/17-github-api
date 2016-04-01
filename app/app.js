68cc8a1897e47b06d24d1ec3b146e4306d6177a0
Nick Garlington <nickgarlington@Nicks-MacBook-Pro-2.local>
2016-04-01 14:20:28 -0500

    almost done

--- app/app.js
+++ app/app.js
@@ -1,2 +1,24 @@
 import 'es6-promise';
 import 'whatwg-fetch';
+
+
+fetch(" https://api.github.com/users/nickgarlington")
+  .then((response) => {
+    return response.json();
+  })
+  .then((response) => {
+    console.log(response);
+
+    document.querySelector("#avatar").src = response.avatar_url;
+    document.querySelector("#name").textContent = response.name;
+    document.querySelector("#username").textContent = response.login;
+    document.querySelector("#location").textContent = response.location;
+    let blog = document.querySelector("#blog")
+    blog.textContent = response.blog;
+    blog.href = response.blog;
+
+    let email = document.querySelector("#email")
+    email.textContent = response.email;
+    email.href = `mailto: ${response.email}`;
+    document.querySelector("#email").textContent = response.email;
+  })
